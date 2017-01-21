8dcbce2bdaa281d52776b17b1902480bd90d7996
Martijn de Milliano <martijn@quicksilver.local>
2017-01-21 20:45:15 +0100

    Finished MVP and styling.

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
+(function() {
+  var cards = require("json!../assets/persian.json");
+  var icard = 0;
+  var flipstate = 0;
+  var wordElement = document.querySelector(".word");
+  var pronounciationElement = document.querySelector(".pronounciation");
 
+  function render() {
+    wordElement.innerHTML = cards[icard][flipstate === 0 ? 'a' : 'b'];
+    pronounciationElement.innerHTML = flipstate === 0 ? cards[icard].p : "";
+  }
+
+  function flip() {
+    flipstate = 1 - flipstate;
+    render();
+  }
+
+  function next() {
+    icard = Math.floor(Math.random() * cards.length);
+    flipstate = Math.random() >= 0.5 ? 1 : 0;
+    render();
+  }
+
+  function init() {
+    document.querySelectorAll('button[data-rel="flip"]').forEach(btn => { btn.addEventListener('click', flip); });
+    document.querySelectorAll('button[data-rel="next"]').forEach(btn => { btn.addEventListener('click', next); });
+    render();
+  }
+
+  init();
+
+})();
